bef79462c56e2f5a92ea4276b80fc6ba743ba40a
maxsondcook <mdc9241@nyu.edu>
2026-05-19 23:34:32 -0700

    Done

--- practice_output.py
+++ practice_output.py
@@ -17,7 +17,7 @@ def print_without_line_break():
     Prints out the text, 'Hello world!' without a line break at the end
     """
     # write your code here
-    print("Hello world!", end="")
+    print("Hello world!", end=" ")
 
 def print_with_separator_dash_and_with_line_break():
     """
@@ -37,4 +37,4 @@ def print_with_separator_dash_and_without_line_break():
     """
     # write your code here
     # you must supply each word as a separate argument to the print() function
-    print("Twas", "brillig", "and", "the", "slithy", "toves", sep="-", end="")
+    print("Twas", "brillig", "and", "the", "slithy", "toves", sep="-", end=" ")
